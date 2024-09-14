7ff351198130619f2da8c267a34700b71bb06f33
Apple <apple@Apples-MacBook-Air.local>
2024-09-14 22:45:19 +0530

    first commit

---
+++ simple_pallindrome.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main() {
+    int t;
+    cin >> t; 
+    while(t--) {
+       int n;
+       cin>>n;
+       string ans="";
+       vector<char> v={'a','e','i','o','u'};
+       if(n<=5){
+           for(int i=0;i<n;i++){
+                ans += v[i];
+           }
+           cout<<ans<<"\n";
+       }else{
+           int k=n%5;
+           int l=n/5;
+           string aa="",bb="",cc="",dd="",ee="";
+           for(int i=0;i<l;i++){
+               aa+='a';
+               bb+='e';
+               cc+='i';
+               dd+='o';
+               ee+='u';
+           }
+           
+           for(int j=0;j<k;j++){
+               if(j==0){
+                   aa+='a';
+               }else if(j==1){
+                   bb+='e';
+               }else if(j==2){
+                   cc+='i';
+               }else if(j==3){
+                   dd+='o';
+               }else if(j==4){
+                   ee+='u';
+               }
+           }
+           string hehe=aa+bb+cc+dd+ee;
+           cout<<hehe<<"\n";
+       }
+    }
+}
